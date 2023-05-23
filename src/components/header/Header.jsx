c3648cfc99bee29ba7404a4a4e7ec2a0b16df898
Will H <will.wkhu@gmail.com>
2023-05-22 22:59:14 -0700

    Create page header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import './Header.css';
+
+function Header({ level, score, top }) {
+  return (
+    <header>
+      <h1>Space Cards Memory Game</h1>
+      <p>
+        Get points by clicking on on image but don&apos;t click a card more than
+        once!
+      </p>
+      <div>
+        <ul>
+          <li>Level: {level}</li>
+          <li>Current Score: {score}</li>
+          <li>Top Score: {top}</li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
