950a68505a2ef7349af93a58047dabd6ab01ef56
Will H <will.wkhu@gmail.com>
2023-05-22 22:57:55 -0700

    Add header component and some state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
-  return <></>;
+  const [level, setLevel] = useState(1);
+  const [score, setScore] = useState(0);
+  const [topScore, setTopScore] = useState(0);
+
+  return (
+    <>
+      <Header level={level} score={score} top={topScore} />
+    </>
+  );
 }
 
 export default App;
