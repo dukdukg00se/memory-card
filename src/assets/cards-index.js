484ab6b0350d0c958291003833c2172f94dd6ba1
Will H <will.wkhu@gmail.com>
2023-05-24 13:14:59 -0700

    Move file

---
+++ src/assets/cards-index.js
@@ -0,0 +1,142 @@
+import uniqid from 'uniqid';
+
+const planets = [
+  {
+    src: './cards/51 Pegasi b.png',
+    id: uniqid(),
+  },
+  {
+    src: './cards/55 Cancri e.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/AG Carinae.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Blackhole.webp',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Brown dwarf.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Carina Nebula.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Ceres.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Curiosity Rover.webp',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Earth.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/EPIC 220674823 b.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/GJ 1132b.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/GJ 3470b.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/HD 189733b.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/HD 80606b.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Horsehead Nebula.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/HR_8799e.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Hubble.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Jupiter.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Luhman 16A.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Mars.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Mercury.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Messier 106.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Messier 57.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Moon.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Neptune.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/NGC 2841.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/NGC 6217.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Pluto.webp',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Saturn.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Sun.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Bubble Nebula.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/UGC 2885.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Uranus.jpg',
+    id: uniqid(),
+  },
+  {
+    src: './cards/Venus.jpg',
+    id: uniqid(),
+  },
+];
+
+export default planets;
